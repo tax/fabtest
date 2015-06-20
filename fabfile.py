d5b8d93c45922af412eb271a8385c57f475b1c2f
Paul <paultax@gmail.com>
2015-06-20 22:49:01 +0200

    more paths

--- fabfile.py
+++ fabfile.py
@@ -2,18 +2,24 @@ from fabric.api import *  # noqa
 from fabric.contrib.files import exists
 
 env.user = 'root'
-env.path_app = '/var/www/test/'
-env.path_virtualenv = env.path_app + 'env/'
-env.git_repo = 'git@github.com:tax/fabtest.git'
+env.app_name = 'fabtest'
+env.path_app = '/var/www/test'
+env.path_repo = '%s/fabtest' % env.path_app
+env.path_virtualenv = '%s/env' % env.path_app
+env.git_repo = 'https://github.com/tax/fabtest.git'#'git@github.com:tax/fabtest.git'
+env.packages = ['redis-server', 'python-dev', 'python-pip']
 
 
 @task
 def deploy(full=True):
+    run('mkdir -p %s' % env.path_app)
+    execute(stop_supervisor)
     execute(install_repo)
     if full:
         execute(install_os_packages)
         execute(create_virtualenv)
         execute(install_python_packages)
+    execute(start_supervisor)
 
 
 @task
@@ -23,34 +29,24 @@ def deploy_fast():
 
 @runs_once
 def install_repo():
-    run('mkdir -p {0}'.format(env.path_app))
-    with cd(env.app_path):
-        if not exists('%s/.git' % env.path_app):
+    if not exists(env.path_repo):
+        with cd(env.path_app):
             run('git clone %s' % env.git_repo)
-        else:
+    else:
+        with cd(env.path_repo):
             run('git pull origin')
 
 
 @runs_once
 def install_os_packages():
-    packages = ['redis-server', 'python-dev', 'python-pip']
-    for package in packages:
+    for package in env.packages:
         sudo('apt-get install -y %s' % package)
 
 
 @runs_once
 def install_python_packages():
     """Install pip requirements in the virtualenv."""
-    with cd(env.app_path):
-        run('%s/bin/pip install -U -r requirements.txt' % env.path_virtualenv)
-
-
-def stop_supervisor():
-    pass
-
-
-def start_supervisor():
-    pass
+    run('%s/bin/pip install -U -r %s/requirements.txt' % (env.path_virtualenv, env.path_repo))
 
 
 @runs_once
@@ -73,3 +69,11 @@ def create_virtualenv():
 def manage(virtualenv, cmd):
     """Run a manage.py command."""
     run('%s/bin/manage.py %s' % (env.path_virtualenv, cmd))
+
+
+def stop_supervisor():
+    pass
+
+
+def start_supervisor():
+    pass
