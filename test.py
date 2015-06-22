6254024fc9c7b7693e7ebbee5dd3bcf9210fcafd
Paul <paultax@gmail.com>
2015-06-22 20:36:43 +0200

    Ow

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(1 + 3, 4)
 
     def test_divide(self):
-        self.assertEqual(8 / 2, 5)
+        self.assertEqual(8 / 2, 4)
 
 if __name__ == '__main__':
     unittest.main()
